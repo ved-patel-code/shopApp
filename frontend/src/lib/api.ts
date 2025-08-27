2abf13d0fe99598b041d3ca5b76e9cd329a4f1de
Samarth Patel <psamarth8836@gmail.com>
2025-08-27 17:13:55 +0530

    change in api.ts had in qouts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "process.env.NEXT_PUBLIC_API_URL", // Your backend URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Your backend URL
   headers: {
     "Content-Type": "application/json",
   },
